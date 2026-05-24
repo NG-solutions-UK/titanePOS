53687069e4d3aad6c5b875d1e5255bfd379c6143
faizankhalid-fk <84088902+faizankhalid-fk@users.noreply.github.com>
2026-05-24 03:58:02 +0100

    stash

--- resources/views/activity-logs/index.blade.php
+++ resources/views/activity-logs/index.blade.php
@@ -7,7 +7,7 @@
     <div class="card">
 
         <div class="card-header">
-            <h4>Activity Logs</h4>
+            <h4>Activity Logss</h4>
         </div>
 
         <div class="card-body table-responsive">
